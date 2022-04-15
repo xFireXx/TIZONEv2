f914c19d2dff209d9123204541c6bdf613a9cfcf
Maciej Treder <treder.mc@gmail.com>
2022-04-15 13:13:32 +0200

    start projektu

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".bar");
+const navbar = document.querySelector(".navbar");
+
+btn.addEventListener("click", () => {
+    navbar.classList.toggle("active-navbar");
+})
