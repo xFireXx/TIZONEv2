96f24cd23d6e8a5219314853081a81c101e1e4e8
Maciej Treder <treder.mc@gmail.com>
2022-05-03 20:33:55 +0200

    nav in panel

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".bar");
-const navbar = document.querySelector(".navbar");
-
-btn.addEventListener("click", () => {
-    navbar.classList.toggle("active-navbar");
-});
-
